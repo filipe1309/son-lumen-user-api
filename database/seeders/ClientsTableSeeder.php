4b00cbbfb52315c643d4d5ef0410ca174db859e5
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-04-17 00:07:49 -0300

    feat: add client entity

--- database/seeders/ClientsTableSeeder.php
+++ database/seeders/ClientsTableSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Client;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class ClientsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -13,7 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
-        $this->call(ClientsTableSeeder::class);
+        Client::factory()->count(20)->create();
     }
 }
